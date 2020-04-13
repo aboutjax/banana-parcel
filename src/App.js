931f8ee6ae65c8d96ca04c2f261c358ebd96861d
Jacky Lee <aboutjax1990@gmail.com>
2020-04-13 22:41:06 +1200

    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  background: red;
+`;
+
+const App = () => {
+  return <StyledApp>body</StyledApp>;
+};
+
+export { App };
